c83127f870936adf72ee2fa16e8371c1454f2c22
ayveezub <ayveezub@gmail.com>
2022-06-10 12:13:11 +0300

    feat(steam.nix): add protontricks

--- extra-packages/steam.nix
+++ extra-packages/steam.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   # Enable steam:
@@ -7,4 +7,9 @@
   # On 64-bit systems, whether to support Direct Rendering for 32-bit applications (such as Wine).
   # This is currently only supported for the `nvidia` as well as `Mesa`.
   hardware.opengl.driSupport32Bit = true;
+
+
+  environment.systemPackages = with pkgs; [
+    protontricks # a simple wrapper for running Winetricks commands for Proton-enabled games
+  ];
 }
